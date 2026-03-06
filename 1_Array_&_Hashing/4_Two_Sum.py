4e2ed81d85099dbace2e2e29a719fa4d5bce06ff
Aniket511 <aniket.343yadav@gmail.com>
2026-03-06 22:06:50 +0530

    new

--- 1_Array_&_Hashing/4_Two_Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        hashmap = {}
-
-        for idx, number in enumerate(nums):
-            difference = target - number
-            if difference in hashmap:
-                return [hashmap[difference], idx]
-            hashmap[number] = idx
